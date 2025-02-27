83053f5a2ba02baad604e3b210c4e66978662ef4
Tamás Szita <szita999@gmail.com>
2025-02-27 23:18:43 +0100

    services comment out in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 import HeroSections from "@/components/HeroSections";
 import Navbar from "@/components/Navbar";
-import { Services } from "@/components/Services";
+// import { Services } from "@/components/Services";
 
 export default function Home() {
   return (
     <div className="">
       <Navbar />
       <HeroSections />
-      <Services />
+      {/* <Services /> */}
     </div>
   );
 }
